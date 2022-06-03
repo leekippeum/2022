a15ffbca31db043d03fe47b5af545402074b3b28
leekippeum <97557249+leekippeum@users.noreply.github.com>
2022-06-03 23:57:23 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+
+    function openNav() {
+        document.getElementById("snb").style.width = "300px";
+    }
+                    
+    function closeNav() {
+        document.getElementById("snb").style.width = "0";
+    }
+
